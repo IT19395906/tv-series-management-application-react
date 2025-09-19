f7ce8f096f0099309a0822d6e1532c19b9c039be
IT19395906 <IT19395906@my.sliit.lk>
2025-09-19 22:30:24 +0530

    fetch with then then and sent data as json.stringify object

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,15 +1,12 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Select from "react-select/base";
 import { toast, ToastContainer } from "react-toastify";
 import Swal from "sweetalert2";
 
 function AddItem() {
 
-    const languages = [
-        { value: 'english', label: 'English' },
-        { value: 'french', label: 'French' },
-        { value: 'german', label: 'German' },
-        { value: 'sinhala', label: 'Sinhala' }];
+    const [categories, setCategories] = useState([]);
+    const [languages, setLanguages] = useState([]);
 
     const [touched, setTouched] = useState({
         category: false,
@@ -37,15 +34,31 @@ function AddItem() {
         tag: '',
     });
 
+    useEffect(() => {
+        fetch('http://localhost:8080/api/tvseries/categories', { method: 'GET' }) //fetch() returns a Promise that resolves to a Response object we handle it using then
+            .then(response => response.json()) //response.json() also returns a Promise we handle it using another then
+            .then(data => { setCategories(data); })
+            .catch(error => toast.error(error.message));
+
+        fetch('http://localhost:8080/api/tvseries/languages', { method: 'GET' })
+            .then(response => response.json())
+            .then(data => { setLanguages(data); })
+            .catch(error => toast.error(error.message));
+    }, []);
+
+    // useEffect(() => {
+    //     console.log('Updated languages:', languages);
+    // }, [languages]);
+
     const dateRef = useRef();
     const handleDate = () => {
         dateRef.current.showPicker?.();
     }
 
     const handleChange = (e) => {
-        const id = e.target.id;
+        const name = e.target.name;
         const value = e.target.value;
-        setFormData(prev => ({ ...prev, [id]: value }));
+        setFormData(prev => ({ ...prev, [name]: value }));
     };
 
     const handleBlur = (e) => {
@@ -76,16 +89,44 @@ function AddItem() {
             cancelButtonText: 'No'
         }).then(result => {
             if (result.isConfirmed) {
-                try {
-                    const res = fetch('http://localhost:8080/api/tvseries/add', { method: 'POST', body: formData });
-                    if (res.message == 'Tv Series Uploaded Successfully') {
-                        toast.success('Tv Series Uploaded Successfully', { toastId: "form-success" })
-                    } else {
-                        toast.error('Tv Series Upload Failed')
-                    }
-                } catch (error) {
-                    toast.error(error, { toastId: "form-error" })
-                }
+                fetch('http://localhost:8080/api/tvseries/add', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(formData) })
+                    .then(response => {
+                        if (!response.ok) throw new Error('Http error');
+                        return response.json();
+                    })
+                    .then(result => {
+                        if (result.message == 'Tv Series Uploaded Successfully') {
+                            toast.success('Tv Series Uploaded Successfully', { toastId: "form-success" })
+                            setFormData({
+                                category: '',
+                                status: 'Ongoing',
+                                title: '',
+                                quality: '',
+                                format: '',
+                                imdb: '',
+                                ro: '',
+                                language: '',
+                                description: '',
+                                releasedDate: '',
+                                seasons: '',
+                                episodes: '',
+                                img: '',
+                                trailer: '',
+                                tag: ''
+                            });
+                            setTouched({
+                                category: false,
+                                title: false,
+                                language: false,
+                                description: false,
+                                releasedDate: false,
+                                img: false
+                            });
+                        } else {
+                            toast.error('Tv Series Upload Failed')
+                        }
+                    })
+                    .catch(error => toast.error(error.message, { toastId: "form-error" }));
             }
         })
 
@@ -97,29 +138,25 @@ function AddItem() {
                 <div className="card-header text-center bg-primary text-white"><h4>Upload Tv Series</h4></div>
                 <div className="card-body">
                     <form onSubmit={handleSubmit}>
-                        <ToastContainer hideProgressBar stacked theme="colored" closeOnClick />
+                        <ToastContainer hideProgressBar stacked theme="colored" closeOnClick autoClose={3000} />
                         <div className="row">
                             <div className="mb-3">
                                 <label htmlFor="category" className="form-label"><b>Category*</b></label>
-                                <select id="category" className={`form-select ${touched.category && !formData.category ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} value={formData.category} required>
+                                <select id="category" name="category" className={`form-select ${touched.category && !formData.category ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} value={formData.category} required>
                                     <option value="" disabled>Select a category</option>
-                                    <option value="Action">Action</option>
-                                    <option value="Drama">Drama</option>
-                                    <option value="Comedy">Comedy</option>
-                                    <option value="Thriller">Thriller</option>
-                                    <option value="Horror">Horror</option>
+                                    {categories.sort().map((cat, index) => <option key={index} value={cat}>{cat}</option>)}
                                 </select>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="title" className="form-label"><b>Title*</b></label>
-                                <input type="text" id="title" className={`form-control ${touched.title && formData.title?.trim() === '' ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required maxLength="50" value={formData.title}
+                                <input type="text" id="title" name="title" className={`form-control ${touched.title && formData.title?.trim() === '' ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required maxLength="50" value={formData.title}
                                     placeholder="Enter TV Series title" />
                             </div>
 
                             <div className="mb-3 col-lg-6 col-md-12 col-sm-12">
                                 <label htmlFor="quality" className="form-label"><b>Quality</b></label>
-                                <select id="quality" className="form-select" onChange={handleChange} value={formData.quality}>
+                                <select id="quality" name="quality" className="form-select" onChange={handleChange} value={formData.quality}>
                                     <option value="" disabled>Select a quality</option>
                                     <option value="480p">480p (SD)</option>
                                     <option value="720p">720p (HD)</option>
@@ -131,7 +168,7 @@ function AddItem() {
 
                             <div className="mb-3 col-lg-6 col-md-12 col-sm-12">
                                 <label htmlFor="format" className="form-label"><b>Format</b></label>
-                                <select id="format" className="form-select" onChange={handleChange} value={formData.format}>
+                                <select id="format" name="format" className="form-select" onChange={handleChange} value={formData.format}>
                                     <option value="" disabled>Select a format</option>
                                     <option value="WEB-DL">WEB-DL</option>
                                     <option value="WEBRip">WEBRip</option>
@@ -147,7 +184,7 @@ function AddItem() {
 
                             <div className="mb-3">
                                 <label htmlFor="language" className="form-label"><b>Language*</b></label>
-                                <Select id="language" onChange={handleLanguageChange} onBlur={handleBlur} value={formData.language}
+                                <Select id="language" name="language" onChange={handleLanguageChange} onBlur={handleBlur} value={formData.language}
                                     options={languages} isSearchable className={`${touched.language && !formData.language ? 'is-invalid' : ''}`} onMenuOpen={() => { }}
                                     onInputChange={() => { }} placeholder="Select a language">
                                 </Select>
@@ -155,48 +192,48 @@ function AddItem() {
 
                             <div className="mb-3">
                                 <label htmlFor="description" className="form-label"><b>Description*</b></label>
-                                <textarea id="description" className={`form-control ${touched.description && formData.description?.trim() === '' ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} value={formData.description} required
+                                <textarea id="description" name="description" className={`form-control ${touched.description && formData.description?.trim() === '' ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} value={formData.description} required
                                     style={{ resize: 'none' }} rows="4" placeholder="Enter a brief description of the TV Series"></textarea>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="releasedDate" className="form-label"><b>Release Date*</b></label>
-                                <input type="date" id="releasedDate" ref={dateRef} onClick={handleDate} value={formData.releasedDate} className={`form-control ${touched.releasedDate && !formData.releasedDate ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required />
+                                <input type="date" id="releasedDate" name="releasedDate" ref={dateRef} onClick={handleDate} value={formData.releasedDate} className={`form-control ${touched.releasedDate && !formData.releasedDate ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="seasons" className="form-label"><b>Total Number of Seasons</b></label>
-                                <input type="number" id="seasons" value={formData.seasons} className="form-control" onChange={handleChange} min="1"
+                                <input type="number" id="seasons" name="seasons" value={formData.seasons} className="form-control" onChange={handleChange} min="1"
                                     max="50" placeholder="Enter Total Number of Seasons" />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="episodes" className="form-label"><b>Total Number of Episodes</b></label>
-                                <input type="number" id="episodes" value={formData.episodes} className="form-control" onChange={handleChange} min="1"
+                                <input type="number" id="episodes" name="episodes" value={formData.episodes} className="form-control" onChange={handleChange} min="1"
                                     max="10000" placeholder="Enter Total Number of Episodes" />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="img" className="form-label"><b>Image*</b></label>
-                                <input type="file" id="img" className={`form-control ${touched.img && !formData.img ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required value={formData.img}
+                                <input type="file" id="img" name="img" className={`form-control ${touched.img && !formData.img ? 'is-invalid' : ''}`} onChange={handleChange} onBlur={handleBlur} required value={formData.img}
                                     accept="image/*" />
                             </div>
 
                             <div className="mb-3 col-lg-6 col-md-12 col-sm-12">
                                 <label htmlFor="imdb" className="form-label"><b>IMDB Rate</b></label>
-                                <input type="number" id="imdb" value={formData.imdb} className="form-control" onChange={handleChange} min="1"
+                                <input type="number" id="imdb" name="imdb" value={formData.imdb} className="form-control" onChange={handleChange} min="1"
                                     max="10" placeholder="Enter IMDB Rate" />
                             </div>
 
                             <div className="mb-3 col-lg-6 col-md-12 col-sm-12">
                                 <label htmlFor="ro" className="form-label"><b>Rotten Tomatoes</b></label>
-                                <input type="number" id="ro" value={formData.ro} className="form-control" onChange={handleChange} min="1"
+                                <input type="number" id="ro" name="ro" value={formData.ro} className="form-control" onChange={handleChange} min="1"
                                     max="100" placeholder="Enter Rotten Tomatoes" />
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="trailer" className="form-label"><b>Youtube Trailer</b></label>
-                                <input type="text" id="trailer" className="form-control" onChange={handleChange} onBlur={handleBlur} maxLength="200" value={formData.trailer}
+                                <input type="text" id="trailer" name="trailer" className="form-control" onChange={handleChange} onBlur={handleBlur} maxLength="200" value={formData.trailer}
                                     placeholder="Enter URL" />
                             </div>
 
@@ -208,9 +245,9 @@ function AddItem() {
                                     <label htmlFor="ongoing" className="form-check-label">Ongoing</label>
                                 </div>
                                 <div className="form-check form-check-inline">
-                                    <input type="radio" id="complete" name="status" className="form-check-input" onChange={handleChange}
-                                        value="Complete" checked={formData.status === 'Completed'} />
-                                    <label htmlFor="complete" className="form-check-label">Complete</label>
+                                    <input type="radio" id="completed" name="status" className="form-check-input" onChange={handleChange}
+                                        value="Completed" checked={formData.status === 'Completed'} />
+                                    <label htmlFor="completed" className="form-check-label">Completed</label>
                                 </div>
                             </div>
 
