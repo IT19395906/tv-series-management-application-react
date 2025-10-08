3e0f3c86078327a523d1fb33f6c47719e7ca22de
IT19395906 <rwijayabandu@gmail.com>
2025-10-08 23:30:58 +0530

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,30 @@ import './App.css'
 import Home from './components/Home'
 import Detail from './components/Detail'
 import Register from './components/Register'
-import {Routes,Route, useLocation, Navigate} from 'react-router-dom'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import Login from './components/Login'
 import Sidebar from './components/Sidebar'
 import ViewAll from './components/ViewAll'
+import NotFound from './components/NotFound'
 
 function App() {
-const location = useLocation();
-const hideNav = ['/login','/register'];
-const showNav = !hideNav.includes(location.pathname);
-  
-return (
+  const location = useLocation();
+  const hideNav = ['/login', '/register'];
+  const showNav = !hideNav.includes(location.pathname);
+
+  return (
     <>
-    {showNav && <Sidebar/>}
-    <Routes>
-      <Route path='/' element={<Navigate to="/login" replace/>}/>
-      <Route path='/home' element={<Home/>} />
-      <Route path='/add' element={<AddItem/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register/>} />
-      <Route path='/detail/:id' element={<Detail/>} />
-      <Route path='/view' element={<ViewAll/>} />
-    </Routes>
+      {showNav && <Sidebar />}
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" replace />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/add' element={<AddItem />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/detail/:id' element={<Detail />} />
+        <Route path='/view' element={<ViewAll />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
