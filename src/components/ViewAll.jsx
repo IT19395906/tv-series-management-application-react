4f48a5a75c604983e8da3eb595915b563d13eaf9
IT19395906 <rwijayabandu@gmail.com>
2025-10-09 22:31:59 +0530

    edit and delete button added

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -203,7 +203,7 @@ function ViewAll() {
                             <td>{series.seasons}</td>
                             <td>{series.episodes}</td>
                             <td>{series.language}</td>
-                            <td>{series.Id}</td>
+                            <td className='d-flex gap-2'><button className='btn btn-primary btn-sm'><i className="fa fa-edit"></i></button><button className='btn btn-danger btn-sm'><i className="fa fa-trash"></i></button></td>
                         </tr>
                     ))}
                 </tbody>
