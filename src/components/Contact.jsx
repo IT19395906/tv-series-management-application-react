4e3497b588e2abdefd736871ff32efb1be053aaa
IT19395906 <rwijayabandu@gmail.com>
2025-10-15 13:36:33 +0530

    improved form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ToastContainer } from 'react-toastify';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    contact: '',
+    content: '',
+    file: null
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files, type } = e.target;
+    setFormData(prev => ({
+      ...prev, [name]: type === 'file' ? files[0] : value
+    }));
+  }
+
   return (
     <div className="card mx-auto rounded shadow">
       <div className="card-header text-center"><h4>Contact Us</h4></div>
@@ -12,34 +28,34 @@ const Contact = () => {
 
             <div className="mb-3">
               <label htmlFor='fname' className='form-label'><b>First Name*</b></label>
-              <input type='text' id='fname' name='fname' className='form-control' maxLength="50" required placeholder='Enter First Name'/>
+              <input type='text' id='fname' name='fname' className='form-control' maxLength="50" required placeholder='Enter First Name' onChange={handleChange} value={formData.fname} />
             </div>
 
             <div className="mb-3">
               <label htmlFor='lname' className='form-label'><b>Last Name*</b></label>
-              <input type='text' id='lname' name='lname' className='form-control' maxLength="50" required placeholder='Enter Last Name'/>
+              <input type='text' id='lname' name='lname' className='form-control' maxLength="50" required placeholder='Enter Last Name' onChange={handleChange} value={formData.lname} />
             </div>
 
             <div className="mb-3">
               <label htmlFor='email' className='form-label'><b>Email*</b></label>
-              <input type='email' id='email' name='email' className='form-control' maxLength="50" required placeholder='Enter Your Email'/>
+              <input type='email' id='email' name='email' className='form-control' maxLength="50" required placeholder='Enter Your Email' onChange={handleChange} value={formData.email} />
             </div>
 
             <div className="mb-3">
               <label htmlFor='contact' className='form-label'><b>Contact Number</b></label>
-              <input type='tel' id='contact' name='contact' className='form-control' maxLength="12" placeholder='Enter Your Contact Number'/>
+              <input type='tel' id='contact' name='contact' pattern="[0-9]{10}" className='form-control' maxLength="12" placeholder='Enter Your Contact Number' onChange={handleChange} value={formData.contact} />
             </div>
 
             <div className="mb-3">
               <label htmlFor="content" className="form-label"><b>Content*</b></label>
-              <textarea id="content" name="content" className="form-control" required
+              <textarea id="content" name="content" className="form-control" required onChange={handleChange} value={formData.content}
                 style={{ resize: 'none' }} rows="5" placeholder="Enter Content of The Message"></textarea>
             </div>
 
             <div className="mb-3">
-              <label htmlFor="img" className="form-label"><b>Uploads</b></label>
-              <input type="file" id="img" name="img" className="form-control"
-                accept="image/*" />
+              <label htmlFor="file" className="form-label"><b>Uploads</b></label>
+              <input type="file" id="file" name="file" className="form-control" onChange={handleChange} value={formData.file}
+                accept="image/*, .txt, .doc, .docx" />
             </div>
 
             <div className="text-center">
