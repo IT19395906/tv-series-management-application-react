5ac8d39804c4b7275603d5b7cc310cfedebcd1a6
IT19395906 <rwijayabandu@gmail.com>
2025-10-23 23:04:14 +0530

    logout button added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,7 +42,10 @@ function Sidebar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <button className="btn btn-dark" onClick={toggleTheme}>{theme === 'light' ? '🌙 Dark' : '☀️ Light'}</button>
+                    <div>
+                        <button className="btn btn-dark" onClick={toggleTheme}>{theme === 'light' ? '🌙 Dark' : '☀️ Light'}</button>
+                        {localStorage.getItem('jwtToken') && (<button className='btn btn-outline-danger ms-2' onClick={logout}>🔒 Logout</button>)}
+                    </div>
                     <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style={{ maxWidth: '9rem' }}>
                         <div className="offcanvas-header" style={{ paddingBottom: '0px' }}>
                             <h3 className="offcanvas-title" id="offcanvasNavbarLabel"><Link className="text-dark" to="/home"><i className='fa fa-home'></i></Link></h3>
